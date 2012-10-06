afc22f79d8181996e890c89bd8541ad8c2d665b4
Arlo O'Keeffe <mailtoarlo@gmail.com>
2012-10-06 07:42:56 -0600

    Remove yuicompressor; use lessc's bultin yuicompressor

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 lessc stylesheets/main.less stylesheets/main.css
-# get hash of the main.css file
-java -jar ./tools/yuicompressor.jar -o stylesheets/intermediate.css stylesheets/main.css
+lessc -yui-compress stylesheets/main.less stylesheets/intermediate.css
 HASH=`md5 -q stylesheets/intermediate.css`
 mv "stylesheets/intermediate.css" "stylesheets/${HASH}.css"
