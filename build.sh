95081da4fd443f3988bca192ab00426e87a4062f
Arlo O'Keeffe <mailtoarlo@gmail.com>
2012-10-10 20:13:33 -0600

    Added build step to replace all stylesheets with minimized version

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ rm stylesheets/min/*.css
 lessc -yui-compress stylesheets/main.less stylesheets/min/intermediate.css
 HASH=`md5 -q stylesheets/min/intermediate.css`
 mv "stylesheets/min/intermediate.css" "stylesheets/min/${HASH}.css"
+#search all html files and exchange stylesheet links
+find . -name "*.html" -exec sed -i "s/<link rel=\"stylesheet\" href=\"stylesheets\/min\/.*\.css\">/<link rel=\"stylesheet\" href=\"stylesheets\/min\/${HASH}\.css\">/g" {} \;
