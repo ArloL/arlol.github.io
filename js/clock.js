97d41f7846c9913c3eadb67fc7ea6133c17d9fa1
Arlo O'Keeffe <mailtoarlo@gmail.com>
2012-10-23 23:29:28 -0600

    Use actual time, resize clock

--- js/clock.js
+++ js/clock.js
@@ -1,78 +1,86 @@
 var R = Raphael('clock');
 
+var clockRadius = Math.min(jQuery('#clock').width() / 2, jQuery('#clock').height());
+
+var center = new Object();
+center.x = clockRadius;
+center.y = clockRadius + 10;
+
+var handleWidth = 0.05 * clockRadius;
+var secondsOuterWidth = 0.02 * clockRadius;
+var secondsHandleWidth = 0.01 * clockRadius;
+
+var set = R.set();
+
 // clock base
-R.circle(300, 300, 200);
+set.push(R.circle(center.x, center.y, clockRadius).attr({
+    fill: 'white',
+    'stroke-width': 0
+}));
 
 var degrees = 0;
 for (var i = 0; i < 12; i++) {
-	R.rect(295, 110, 10, 30).attr({
+	set.push(R.rect(center.x - (handleWidth / 2), center.y - clockRadius + 0.05 * clockRadius, handleWidth, 0.15 * clockRadius).attr({
 		fill: 'black',
-		transform: 'r' + degrees + ',300,300'
-	});
+		transform: 'r' + degrees + ',' + center.x + ',' + center.y
+	}));
 	degrees += 30;
 };
 
 degrees = 0;
 for (var i = 0; i < 60; i++) {
-	R.rect(298, 110, 4, 10).attr({
+	set.push(R.rect(center.x - (secondsOuterWidth / 2), center.y - clockRadius + 0.05 * clockRadius, secondsOuterWidth, 0.05 * clockRadius).attr({
 		fill: 'black',
-		transform: 'r' + degrees + ',300,300'
-	});
+		transform: 'r' + degrees + ',' + center.x + ',' + center.y
+	}));
 	degrees += 6;
 };
 
+var date = new Date();
+
+var initialSeconds = date.getSeconds() / 60 * 360 + date.getMilliseconds() / 60 / 1000 * 360;
+var initialMinutes = date.getMinutes() / 60 * 360 + initialSeconds / 60;
+var initialHours = date.getHours() / 12 * 360 + initialMinutes / 12;
+
+var hoursRotation = 30 * 360;
+var minutesRotation = hoursRotation * 12;
+var secondsRotation = minutesRotation * 60;
+var milliseconds = secondsRotation / 6 * 1000;
+
 // hours
-var hours = R.rect(250, 295, 150, 10).attr({
-	fill: 'black'
-})
+set.push(R.rect(center.x - (handleWidth / 2), center.y - clockRadius * 0.5, handleWidth, clockRadius * 0.75).attr({
+	fill: 'black',
+	transform: 'r' + initialHours + ',' + center.x + ',' + center.y
+}).animate({
+    transform: 'r' + hoursRotation + ',' + center.x + ',' + center.y
+}, milliseconds));
 
 // minutes
-var minutes = R.rect(250, 295, 240, 10).attr({
-	fill: 'black'
-});
+set.push(R.rect(center.x - (handleWidth / 2), center.y - clockRadius * 0.95, handleWidth, clockRadius * 1.2).attr({
+	fill: 'black',
+	transform: 'r' + initialMinutes + ',' + center.x + ',' + center.y
+}).animate({
+    transform: 'r' + minutesRotation + ',' + center.x + ',' + center.y
+}, milliseconds));
 
 // seconds
-var seconds = R.rect(250, 299, 180, 2).attr({
+set.push(R.rect(center.x - (secondsHandleWidth / 2), center.y - clockRadius * 0.65, secondsHandleWidth, clockRadius * 0.9).attr({
+	fill: 'red',
+	'stroke-width': 0,
+	transform: 'r' + initialSeconds + ',' + center.x + ',' + center.y
+}).animate({
+    transform: 'r' + secondsRotation + ',' + center.x + ',' + center.y
+}, milliseconds));
+
+set.push(R.circle(center.x, center.y, 0.025 * clockRadius).attr({
 	fill: 'red',
 	'stroke-width': 0
-});
+}));
 
-R.circle(300, 300, 5).attr({
+set.push(R.circle(center.x, center.y - clockRadius * 0.65, clockRadius * 0.075).attr({
 	fill: 'red',
-	'stroke-width': 0
-});
-
-var seconds_circle = R.circle(430, 300, 15).attr({
-	fill: 'red',
-	'stroke-width': 0
-});
-
-function rotate_hours() {
-	hours.animate({
-		transform: 'r3600,300,300'
-	}, 180000);
-}
-
-function rotate_minutes() {
-	minutes.animate({
-		transform: 'r3600,300,300'
-	}, 120000);
-}
-
-function rotate_seconds() {
-	seconds.animate({
-		transform: 'r3600,300,300'
-	}, 60000);
-}
-
-function rotate_seconds_circle() {
-	seconds_circle.animate({
-		transform: 'r3600,300,300'
-	}, 60000);
-}
-
-rotate_hours();
-rotate_minutes();
-rotate_seconds();
-rotate_seconds_circle();
-
+	'stroke-width': 0,
+	transform: 'r' + initialSeconds + ',' + center.x + ',' + center.y
+}).animate({
+    transform: 'r' + secondsRotation + ',' + center.x + ',' + center.y
+}, milliseconds));
