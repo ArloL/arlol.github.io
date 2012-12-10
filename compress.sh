ee526a7e113e102b77afa90520fb1e9dd3d37338
Arlo O'Keeffe <mailtoarlo@gmail.com>
2012-12-10 01:47:31 -0600

    Rename build.sh to compress.sh

--- compress.sh
+++ compress.sh
@@ -5,4 +5,3 @@ HASH=`md5 -q css/intermediate.css`
 mv "css/intermediate.css" "css/${HASH}.css"
 #search all html files and exchange stylesheet links to the new minified version
 find . -name "*.html" -exec sed -i "" "s/<link rel=\"stylesheet\" href=\"\/css\/.*\.css\">/<link rel=\"stylesheet\" href=\"\/css\/${HASH}\.css\">/g" {} \;
-find . -name "*.html" -exec sed -i "" "s/<link rel=\"stylesheet\" href=\"\.\.\/css\/.*\.css\">/<link rel=\"stylesheet\" href=\"\.\.\/css\/${HASH}\.css\">/g" {} \;
