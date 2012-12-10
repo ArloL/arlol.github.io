0298fdb0b61595b78df8ba4e790c25f7819821f8
Arlo O'Keeffe <mailtoarlo@gmail.com>
2012-12-10 01:06:49 -0600

    Update build script to replace absolute paths

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ lessc --yui-compress "_less/main.less" > "css/intermediate.css"
 HASH=`md5 -q css/intermediate.css`
 mv "css/intermediate.css" "css/${HASH}.css"
 #search all html files and exchange stylesheet links to the new minified version
-find . -name "*.html" -exec sed -i "" "s/<link rel=\"stylesheet\" href=\"css\/.*\.css\">/<link rel=\"stylesheet\" href=\"css\/${HASH}\.css\">/g" {} \;
+find . -name "*.html" -exec sed -i "" "s/<link rel=\"stylesheet\" href=\"\/css\/.*\.css\">/<link rel=\"stylesheet\" href=\"\/css\/${HASH}\.css\">/g" {} \;
 find . -name "*.html" -exec sed -i "" "s/<link rel=\"stylesheet\" href=\"\.\.\/css\/.*\.css\">/<link rel=\"stylesheet\" href=\"\.\.\/css\/${HASH}\.css\">/g" {} \;
